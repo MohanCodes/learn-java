a77c9797e6ebf1259a6bb2e18fd6570bb5f2ecbb
MohanHSMN <mohan.atkuri@hsmn.org>
2023-08-24 07:48:27 -0500

    Unit two code commits

---
+++ Unit2UsingObjects/EscapeSequence.java
@@ -0,0 +1,18 @@
+package Unit2UsingObjects;
+
+public class EscapeSequence {
+    public static void main(String[] args) {
+        System.out.println("abc \\ \' def \n \" ghi");
+    }
+}
+
+/*
+\\ Inserts a backslash
+\n Inserts a newline
+\" Inserts a double quote
+\' Inserts a single quote
+\t Inserts a tab
+\b Inserts a backspace
+\r Inserts a carriage return
+\f Inserts a formfeed
+ */
